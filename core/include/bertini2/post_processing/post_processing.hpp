3e8b83f8f2ad7c93acc1db2846fbd65935b8eb92
daniel brake <danielthebrake@gmail.com>
2016-02-16 11:18:33 -0500

    initial skeleton code for post-processing.

---
+++ core/include/bertini2/post_processing/post_processing.hpp
@@ -0,0 +1,39 @@
+//This file is part of Bertini 2.0.
+//
+//post_processing.hpp is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//post_processing.hpp is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with post_processing.hpp.  If not, see <http://www.gnu.org/licenses/>.
+//
+//
+//  Daniel Brake
+//  University of Notre Dame
+//  ACMS
+//  Spring 2016
+// 
+
+/**
+\file post_processing.hpp 
+
+\brief The main file for post-processing utilities in Bertini2.
+*/
+
+
+
+namespace bertini{
+	namespace post_processing {
+
+
+		
+	}
+
+
+}  // re: namespace bertini
